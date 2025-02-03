a38fb8147d61a7fce5201c73ae2987efc4896fe2
cyschneck <22159116+cyschneck@users.noreply.github.com>
2025-02-02 22:23:06 -0700

    setup loading img

--- comic-canopy/scripts/search_discover.gd
+++ comic-canopy/scripts/search_discover.gd
@@ -1,13 +1,15 @@
 extends Control
 
 @onready var input_line: LineEdit = %InputLine
-const TEST_MY_COMICS_GRID_CONTAINER = preload("res://scenes/test_my_comics_grid_container.tscn")
 @onready var discover_v_box_container: VBoxContainer = $DiscoverScrollContainer/DiscoverVBoxContainer
+@onready var loading_image: TextureRect = $LoadingImage
+const TEST_MY_COMICS_GRID_CONTAINER = preload("res://scenes/test_my_comics_grid_container.tscn")
 
 var _BASE_URL = ''
 
 func _ready() -> void:
 	discover_v_box_container.visible = false
+	loading_image.visible = false
 
 func set_base_url(url_input: String) -> void:
 	_BASE_URL = url_input
@@ -18,7 +20,10 @@ func get_base_url() -> String:
 func _on_line_edit_text_submitted(url_input: String) -> void:
 	# triggered when enter pressed
 	input_line.text = "" # remove input after submitted
+	loading_image.visible = true
+	discover_v_box_container.visible = false # set container false for each new search
 	print(url_input)
+
 	if "http://" not in url_input: url_input = "http://" + url_input
 	if url_input[-1] == "/": url_input = url_input.left(url_input.length() - 1) # remove trailing /
 	set_base_url(url_input)
@@ -46,6 +51,7 @@ func _on_request_completed(result, response_code, headers, body) -> void:
 		http_request.request_completed.connect(_on_request_archive_completed)
 		http_request.request(url)
 	else:
+		loading_image.visible = false
 		print("invalid URL")
 
 func _on_request_archive_completed(result, response_code, headers, body) -> void:
@@ -57,11 +63,13 @@ func _on_request_archive_completed(result, response_code, headers, body) -> void
 
 	for line in body_html:
 		if "a href=" in line:
+			print(line)
 			var discover_row = TEST_MY_COMICS_GRID_CONTAINER.instantiate()
 			discover_row.name = line
 			discover_row.get_child(1).text = line
 			# edit link button text
 			#discover_row.get_child(2).uri = "https://xkcd.com/3043/"
-			discover_v_box_container.visible = true
 			discover_v_box_container.add_child(discover_row)
-	print("done")
+
+	loading_image.visible = false
+	discover_v_box_container.visible = true
