8938fb9ea99200459c761dba66a3e502ab60c5fb
cyschneck <22159116+cyschneck@users.noreply.github.com>
2026-05-22 11:03:57 -0600

    populate recommendations based on tags in database

--- recommendations.php
+++ recommendations.php
@@ -47,14 +47,17 @@
 
         <input type="text" placeholder="Search for comic..." id="comicName" onkeyup="searchComicsByName()">
         <section id="tag_grids">
+            <?php
+                $all_tags_query = $pdo->query("SELECT REPLACE(GROUP_CONCAT(Tags), '“', '') from all_comics");
+                $row = $all_tags_query->fetch(PDO::FETCH_ASSOC);
+                $all_tags = implode(", ", $row);
+                $tags_as_str = implode(', ', array_unique(array_map('trim', explode(',', $all_tags))));
+                $tag_array = explode(",", $tags_as_str);
+            ?>
             <ul>
-                <li>Essential Reading</li>
-                <li>Fantasy</li>
-                <li>Science-Fiction</li>
-                <li>Horror</li>
-                <li>Educational</li>
-                <li>Romance</li>
-                <li>Comedy</li>
+                <?php foreach ($tag_array as $tag): ?>
+                    <li><?= $tag ?></li>    
+                <?php endforeach; ?>
             </ul>
         </section>
 
